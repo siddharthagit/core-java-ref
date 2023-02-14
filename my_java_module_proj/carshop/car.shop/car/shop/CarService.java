51e5c77503b42daa30c1ab21ff9851775c900845
Sidd Bhattacharjee <sbhattacharjee@Siddharthas-MacBook-Pro.local>
2023-02-13 19:30:30 -0800

    Delete .DS_Store file

--- my_java_module_proj/carshop/car.shop/car/shop/CarService.java
+++ my_java_module_proj/carshop/car.shop/car/shop/CarService.java
@@ -1,15 +1,17 @@
 package car.shop;
+
+
 import car.common.model.Car;
+
 public class CarService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Car car = new Car();
 		changeTire(car);
 	}
-	
-	public static void changeTire (car.common.model.Car car) {
-		System.out.println("Car Service"); 
+
+	public static void changeTire(car.common.model.Car car) {
+		System.out.println("Car Service");
 	}
 
 }
